50aeb4cb59c325f4597b00e834201fb0046e9955
simin <120188554+inho1019@users.noreply.github.com>
2023-01-06 19:16:16 +0900

    Add files via upload

--- project.b/js/script.js
+++ project.b/js/script.js
@@ -1,18 +1,37 @@
 const sbut = document.querySelector(".sbut");
 const ststr = document.querySelector(".ststr");
 const stsec = document.querySelector(".start");
+const ocsec = document.querySelector(".allocean");
+const whale = document.querySelector(".whale")
+const boat = document.querySelector(".boat")
+const topsec = document.querySelector(".topsec")
 
 let stt = 0;
 let twn = 0;
+let tht = 0;
+let frt = 0;
 
 const seaso = new Audio('seasound.mp3');
 
-    seaso.addEventListener('ended', function() { 
-        this.currentTime = 0;
-        this.play();
-    }, false)
+seaso.addEventListener('timeupdate', function(){
+    var buffer = .44
+    if(this.currentTime > this.duration - buffer){
+        this.currentTime = 0
+        this.play()
+    }
+});
+function vlup (a) {
+    if (a.volume > 0.5) {
+    } else { 
+    setTimeout(() => {
+        a.volume += 0.05;
+        vlup(a);
+            }, 200)
+    }
+}
 
 function start() {
+    //sbut.disabled = true;
     sbut.style.animation = "byebut 4s";
     setTimeout(() => {
         sbut.style.display = "none";
@@ -64,7 +83,7 @@ function twobut(tw) {
         twbtcho[i].classList.remove("animate__fadeIn");
         twbtcho[i].classList.add("animate__animated");
         twbtcho[i].classList.add("animate__fadeOut");
-        twbtcho[i].disabled = true;
+        //twbtcho[i].disabled = true;
         }
         setTimeout(() => {
             stsec.style.paddingBottom = "30%";
@@ -88,6 +107,8 @@ function twbutnext(a,b) {
         if (twn === 1) {
             if (a === 0) {
                 seaso.play();
+                seaso.volume = 0;
+                vlup(seaso);
             } else {
                 stsec.style.paddingBottom = "50%";
                 star.classList.add("animate__animated");
@@ -104,6 +125,77 @@ function twbutnext(a,b) {
         ststr.classList.remove("animate__fadeIn");
         ststr.classList.add("animate__animated");
         ststr.classList.add("animate__fadeOut");
+        setTimeout(() => {
+            thrtext(tht,a);
+        }, 2500);
+    }
+        }, 4000);
+}
+
+function thrtext(a,b) {
+    ststr.innerHTML = thrtx[a]; 
+    ststr.classList.remove("animate__fadeOut");
+    ststr.classList.add("animate__animated");
+    ststr.classList.add("animate__fadeIn");
+    ststr.style.setProperty('--animate-duration', '2s');
+    tht += 1;
+    setTimeout(() => {
+    if (tht < thrtx.length) {
+        ststr.classList.remove("animate__fadeIn");
+        ststr.classList.add("animate__animated");
+        ststr.classList.add("animate__fadeOut");
+         setTimeout(() => { 
+             thrtext(tht,b);
+             ststr.classList.remove("animate__fadeOut");
+                           }, 2000);
+    } else {
+        ststr.classList.remove("animate__fadeIn");
+        ststr.classList.add("animate__animated");
+        ststr.classList.add("animate__fadeOut");
+        setTimeout(() => {
+        if (b === 0) {
+                stsec.style.paddingBottom = "0%";
+            } else {
+                stsec.style.paddingBottom = "60%";
+            }
+            ocsec.style.animation = "fadeio 12s";
+            topsec.style.animation = "fadeio 12s";
+            boat.style.opacity = "0.5";
+            setTimeout(() => {
+                setTimeout(() => {
+                    boat.style.opacity = "1"
+                    }, 5000);
+            frtext(frt);
+            }, 5000);
+        }, 1000);
+    }
+        }, 4000);
+}
+
+function frtext(a) {
+    ststr.classList.remove("animate__fadeOut");
+    ststr.innerHTML = frtx[a]; 
+    ststr.classList.add("animate__animated");
+    ststr.classList.add("animate__fadeIn");
+    ststr.style.setProperty('--animate-duration', '2s');
+    frt += 1;
+    setTimeout(() => {
+    if (frt < frtx.length) {
+        ststr.classList.remove("animate__fadeIn");
+        ststr.classList.add("animate__animated");
+        ststr.classList.add("animate__fadeOut");
+         setTimeout(() => { 
+             frtext(frt);
+             ststr.classList.remove("animate__fadeOut");
+                           }, 2000);
+    } else {
+        ststr.classList.remove("animate__fadeIn");
+        ststr.classList.add("animate__animated");
+        ststr.classList.add("animate__fadeOut");
+        setTimeout(() => {
+            stsec.style.paddingBottom = "30%";
+            whaletxf(wht);//이벤트 발생 지점
+        }, 1000);
     }
         }, 4000);
 }
